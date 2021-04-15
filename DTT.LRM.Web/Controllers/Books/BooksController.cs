1f7343845336d4dc479b920f99f44f04b69f3bea
DESKTOP-6THU9O2\Tri Thuc <trithuc97.tb@gmail.com>
2021-04-16 01:08:04 +0700

    no message

---
+++ DTT.LRM.Web/Controllers/Books/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DTT.LRM.Web.Controllers.Books
+{
+    public class BooksController : LRMControllerBase
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOrUpdate()
+        {
+            return View();
+        }
+    }
+}
